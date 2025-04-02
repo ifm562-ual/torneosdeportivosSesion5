907251c3d4b9b4df628af82ccca11690bc10b6be
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2025-04-02 13:42:02 +0200

    Añadidos diagramas para requisitos.

--- src/main/java/org/ualhmis/torneos/Entrenador.java
+++ src/main/java/org/ualhmis/torneos/Entrenador.java
@@ -3,7 +3,18 @@ package org.ualhmis.torneos;
 import java.time.LocalDate;
 
 class Entrenador extends Persona {
-    public Entrenador(String nombre, String genero, LocalDate fechaNacimiento) {
+	private boolean esPrincipal;
+	
+    public void setEsPrincipal(boolean esPrincipal) {
+		this.esPrincipal = esPrincipal;
+	}
+
+	public boolean getEsPrincipal() {
+		return esPrincipal;
+	}
+
+	public Entrenador(String nombre, String genero, LocalDate fechaNacimiento, boolean esPrincipal) {
         super(nombre, genero, fechaNacimiento);
+        this.esPrincipal = esPrincipal;
     }
 }
